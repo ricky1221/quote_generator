2ba636356710e5f146118585a0ea9283f4bdf4b4
Legumio, Ricardo T <127361251+ricky1221@users.noreply.github.com>
2023-09-05 21:44:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import { generateTextAndImage } from "./utils.js"
 
 // 1. Change the value of the variable to your name
-let name = "Guil Hernandez"
+let name = "Ricky Legumio"
 
 // 2. Change the value of the variable to your favorite activity
-let favoriteActivity = "kayaking"
+let favoriteActivity = "Gaming"
 
 // 3. Assign the favoritePlace variable your favorite place
 // I.e. city, mountain, pub, forrest, beach, Manhattan, etc.
-let favoritePlace = "florida keys"
+let favoritePlace = "Comp Hub International x MPSPC"
 
 // 4. Configure the AI by setting a temperature from 0 to 1
 // The higher temperature, the more random & experimental output
-let temperature = 1
+let temperature = 0.8
 
 // Optional: delete "avatar.jpg" and add a photo of yourself
 // (remember to use "avatar.jpg" as the name of your photo)
